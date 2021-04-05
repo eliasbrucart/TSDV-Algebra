6c3095b2e6a2cb1d5441b4413509a8252e654ce6
Elias <eliasbrucart@hardgames.com.ar>
2021-04-05 11:03:12 -0300

    Added my plane class

---
+++ Assets/Scripts/MathDebbuger/Debuggers/PlaneDebugger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MathDebbuger.Internals;
+
+public class PlaneDebugger : MonoBehaviour
+{
+    Vector3 point = new Vector3(0, 0, 0);
+    Vector3 point2 = new Vector3(5, 0, 0);
+    Vector3 point3 = new Vector3(5, 5, 0);
+    Vector3 point4 = new Vector3(0, 5, 0);
+    Vector3 point5 = new Vector3(0, 5, 5);
+    Vector3 point6 = new Vector3(5, 5, 5);
+    Vector3 point7 = new Vector3(5, 0, 5);
+    Vector3 point8 = new Vector3(0, 0, 5);
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MathDebbuger/PlaneA.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CustomMath
+{
+    public struct PlaneA
+    {
+        public PlaneA(Vec3 inNormal, Vec3 inPoint)
+        {
+
+        }
+
+        public PlaneA(Vec3 inNormal, float d)
+        {
+
+        }
+
+        public PlaneA(Vec3 a, Vec3 b, Vec3 c)
+        {
+
+        }
+
+        public Vec3 normal { get; set; }
+
+        public float distance { get; set; }
+
+        public PlaneA flipped { get; }
+    }
+}
+
