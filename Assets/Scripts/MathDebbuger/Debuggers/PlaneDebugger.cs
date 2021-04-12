b301c6d16fe36cf427681a7fc6d2ef2134bba9a4
Elias <eliasbrucart@hardgames.com.ar>
2021-04-12 10:49:47 -0300

    Check with planes ready

--- Assets/Scripts/MathDebbuger/Debuggers/PlaneDebugger.cs
+++ Assets/Scripts/MathDebbuger/Debuggers/PlaneDebugger.cs
@@ -2,25 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 using MathDebbuger.Internals;
+using System;
 
 public class PlaneDebugger : MonoBehaviour
 {
-    Vector3 point = new Vector3(0, 0, 0);
-    Vector3 point2 = new Vector3(5, 0, 0);
-    Vector3 point3 = new Vector3(5, 5, 0);
-    Vector3 point4 = new Vector3(0, 5, 0);
-    Vector3 point5 = new Vector3(0, 5, 5);
-    Vector3 point6 = new Vector3(5, 5, 5);
-    Vector3 point7 = new Vector3(5, 0, 5);
-    Vector3 point8 = new Vector3(0, 0, 5);
+    private Plane up;
+    private Plane down;
+    private Plane left;
+    private Plane right;
+    private Plane front;
+    private Plane back;
+
+    [SerializeField] private Transform cube;
+
+    Vector3 point1 = new Vector3(-5, -5, -5);
+    Vector3 point2 = new Vector3(5, -5, -5);
+    Vector3 point3 = new Vector3(5, 5, -5);
+    Vector3 point4 = new Vector3(-5, 5, -5);
+    Vector3 point5 = new Vector3(5, -5, 5);
+    Vector3 point6 = new Vector3(-5, -5, 5);
+    Vector3 point7 = new Vector3(-5, 5, 5);
+    Vector3 point8 = new Vector3(5, 5, 5);
+
+    private bool checkUp;
+    private bool checkDown;
+    private bool checkLeft;
+    private bool checkRight;
+    private bool checkFront;
+    private bool checkBack;
     void Start()
     {
-        
+        up = new Plane(point3, point7);
+        down = new Plane(point1, point5);
+        left = new Plane(point1, point7);
+        right = new Plane(point2, point8);
+        front = new Plane(point5, point7);
+        back = new Plane(point1, point3);
     }
 
-    
+
     void Update()
     {
-        
+        checkUp = up.GetSide(cube.transform.position);
+        checkDown = down.GetSide(cube.transform.position);
+        checkLeft = left.GetSide(cube.transform.position);
+        checkRight = right.GetSide(cube.transform.position);
+        checkFront = front.GetSide(cube.transform.position);
+        checkBack = back.GetSide(cube.transform.position);
+
+        if(checkUp && checkDown && checkLeft && checkRight && checkFront && checkDown)
+        {
+            Debug.Log("Esta adentro!");
+        }
+        else
+        {
+            Debug.Log("Esta afuera!");
+        }
+
+        //Debug.Log("Plano UP" + up.GetSide(cube.transform.position).ToString());
+        //Debug.Log("Plano DOWN" + down.GetSide(cube.transform.position).ToString());
+        //Debug.Log("Plano LEFT" + left.GetSide(cube.transform.position).ToString());
+        //Debug.Log("Plano RIGHT" + right.GetSide(cube.transform.position).ToString());
+        //Debug.Log("Plano FRONT" + front.GetSide(cube.transform.position).ToString());
+        //Debug.Log("Plano BACK" + back.GetSide(cube.transform.position).ToString());
     }
 }
--- Assets/Scripts/MathDebbuger/PlaneA.cs
+++ Assets/Scripts/MathDebbuger/PlaneA.cs
@@ -4,28 +4,28 @@ using UnityEngine;
 
 namespace CustomMath
 {
-    public struct PlaneA
-    {
-        public PlaneA(Vec3 inNormal, Vec3 inPoint)
-        {
-
-        }
-
-        public PlaneA(Vec3 inNormal, float d)
-        {
-
-        }
-
-        public PlaneA(Vec3 a, Vec3 b, Vec3 c)
-        {
-
-        }
-
-        public Vec3 normal { get; set; }
-
-        public float distance { get; set; }
-
-        public PlaneA flipped { get; }
-    }
+    //public struct PlaneA
+    //{
+    //    public PlaneA(Vec3 inNormal, Vec3 inPoint)
+    //    {
+    //
+    //    }
+    //
+    //    public PlaneA(Vec3 inNormal, float d)
+    //    {
+    //
+    //    }
+    //
+    //    public PlaneA(Vec3 a, Vec3 b, Vec3 c)
+    //    {
+    //
+    //    }
+    //
+    //    public Vec3 normal { get; set; }
+    //
+    //    public float distance { get; set; }
+    //
+    //    public PlaneA flipped { get; }
+    //}
 }
 
