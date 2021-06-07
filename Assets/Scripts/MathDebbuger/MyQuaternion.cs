741cc38ba6bb42bdde75521b45cf5de9a0ef0ceb
Elias <eliasbrucart@hardgames.com.ar>
2021-06-07 16:21:54 -0300

    Added Equals function to MyQuaternion class

--- Assets/Scripts/MathDebbuger/MyQuaternion.cs
+++ Assets/Scripts/MathDebbuger/MyQuaternion.cs
@@ -1,18 +1,56 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
-public class MyQuaternion : MonoBehaviour
+namespace CustomMath
 {
-    // Start is called before the first frame update
-    void Start()
+    public struct MyQuaternion : IEquatable<MyQuaternion>
     {
-        
-    }
+        public const float kEpsilon = 1E-06F;
+        public float x;
+        public float y;
+        public float z;
+        public float w;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //constructors
+        public MyQuaternion(float x, float y, float z, float w)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.w = w;
+        }
+
+        //public MyQuaternion()
+        //{
+        //    this.x = 0.0f;
+        //    this.y = 0.0f;
+        //    this.z = 0.0f;
+        //    this.w = 0.0f;
+        //}
+
+        public MyQuaternion(MyQuaternion quat)
+        {
+            this.x = quat.x;
+            this.y = quat.y;
+            this.z = quat.z;
+            this.w = quat.w;
+        }
+
+        //operators
+
+        //methods
+        #region Methods
+        public static float Angle(MyQuaternion a, MyQuaternion b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Equals(MyQuaternion other)
+        {
+            return x == other.x && y == other.y && z == other.z && w == other.w;
+        }
+
+        #endregion
     }
 }
+
